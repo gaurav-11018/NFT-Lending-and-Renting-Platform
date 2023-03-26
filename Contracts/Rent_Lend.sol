7dd81b6c91ddc92d5a031b85c8243cd851da899f
Gaurav <gaurav11018@gmail.com>
2023-03-26 14:46:10 +0530

    initial commit

---
+++ Contracts/Rent_Lend.sol
@@ -0,0 +1,127 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract NFTLendingPlatform {
+    struct Loan {
+        address lender;
+        address borrower;
+        uint256 tokenId;
+        uint256 startTime;
+        uint256 duration;
+        uint256 collateralAmount;
+        uint256 feeAmount;
+        bool isActive;
+    }
+
+    mapping(address => mapping(uint256 => Loan)) public loans;
+
+    function lendNFT(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 duration,
+        uint256 collateralAmount,
+        uint256 feeAmount
+    ) external {
+        IERC721 nft = IERC721(nftAddress);
+        require(nft.ownerOf(tokenId) == msg.sender, "Not the owner of the NFT");
+
+        nft.transferFrom(msg.sender, address(this), tokenId);
+
+        loans[nftAddress][tokenId] = Loan({
+            lender: msg.sender,
+            borrower: address(0),
+            tokenId: tokenId,
+            startTime: 0,
+            duration: duration,
+            collateralAmount: collateralAmount,
+            feeAmount: feeAmount,
+            isActive: true
+        });
+    }
+
+    function borrowNFT(
+        address nftAddress,
+        uint256 tokenId,
+        address collateralTokenAddress
+    ) external {
+        Loan storage loan = loans[nftAddress][tokenId];
+        require(loan.isActive, "Loan is not active");
+        require(loan.borrower == address(0), "NFT is already borrowed");
+
+        IERC20 collateralToken = IERC20(collateralTokenAddress);
+        require(
+            collateralToken.balanceOf(msg.sender) >= loan.collateralAmount,
+            "Insufficient collateral balance"
+        );
+        require(
+            collateralToken.allowance(msg.sender, address(this)) >=
+                loan.collateralAmount,
+            "Collateral allowance insufficient"
+        );
+
+        collateralToken.transferFrom(
+            msg.sender,
+            loan.lender,
+            loan.collateralAmount
+        );
+
+        loan.borrower = msg.sender;
+        loan.startTime = block.timestamp;
+    }
+
+    function returnNFT(
+        address nftAddress,
+        uint256 tokenId,
+        address feeTokenAddress
+    ) external {
+        Loan storage loan = loans[nftAddress][tokenId];
+        require(loan.isActive, "Loan is not active");
+        require(loan.borrower == msg.sender, "Not the borrower");
+
+        require(
+            block.timestamp <= loan.startTime + loan.duration,
+            "Loan duration exceeded"
+        );
+
+        IERC20 feeToken = IERC20(feeTokenAddress);
+        require(
+            feeToken.balanceOf(msg.sender) >= loan.feeAmount,
+            "Insufficient fee balance"
+        );
+        require(
+            feeToken.allowance(msg.sender, address(this)) >= loan.feeAmount,
+            "Fee allowance insufficient"
+        );
+
+        feeToken.transferFrom(msg.sender, loan.lender, loan.feeAmount);
+
+        loan.borrower = address(0);
+        loan.startTime = 0;
+    }
+
+    function retrieveNFT(address nftAddress, uint256 tokenId) external {
+        Loan storage loan = loans[nftAddress][tokenId];
+        require(loan.isActive, "Loan is not active");
+        require(loan.lender == msg.sender, "Not the lender");
+        require(loan.borrower == address(0), "NFT is still borrowed");
+
+        IERC721(nftAddress).transferFrom(address(this), msg.sender, tokenId);
+        loan.isActive = false;
+    }
+
+    function withdrawCollateral(
+        address collateralTokenAddress,
+        uint256 amount
+    ) external {
+        IERC20 collateralToken = IERC20(collateralTokenAddress);
+        require(
+            collateralToken.balanceOf(address(this)) >= amount,
+            "Insufficient collateral balance"
+        );
+
+        collateralToken.transfer(msg.sender, amount);
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pink-Ostrich-Contract
 
-
+<!--
 Hey babe if you are reading this just know that I love you
 
-Babe! I had the widest smile when I read this now. Absolutely took me by surprise, I love you sooo much ♥️
+Babe! I had the widest smile when I read this now. Absolutely took me by surprise, I love you sooo much ♥️ -->
